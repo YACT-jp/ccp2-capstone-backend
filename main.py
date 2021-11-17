4b1f0cf7941cd8c0676cab3051cf8467f0c19f08
Norifumi Kimura <norical496@gmail.com>
2021-11-17 23:31:22 +0900

    Setup MongoDB and env variables

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import os
 from flask import Flask
+import pymongo
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
+
+DB_PROJECT_NAME = os.environ.get('DB_PROJECT_NAME')
+DB_NAME = os.environ.get("DB_NAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+
+client = pymongo.MongoClient(
+    f"mongodb+srv://{DB_PROJECT_NAME}:{DB_PASSWORD}@cluster0.y0meq.mongodb.net/{DB_NAME}?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+db = client['ccp2-capstone']
+mediaCollection = db['media']
+
+for media in mediaCollection.find():
+    print(media)
 
 
 @app.route('/')
