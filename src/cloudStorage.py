a8544346d649ab921ce52c3b1b1f16891bf62e3d
Norifumi Kimura <norical496@gmail.com>
2021-12-01 20:58:30 +0900

    Modularized source code and implemented delete photo by object(id) endpoint

---
+++ src/cloudStorage.py
@@ -0,0 +1,23 @@
+from google.cloud import storage
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    return blob.public_url
+
+
+def delete_blob(bucket_name, blob_name):
+    """Deletes a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # blob_name = "your-object-name"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.delete()
